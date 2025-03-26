a08e5ef260ad7b2a83cfc2c1a79c959ba1a61722
Teddy525252 <ausdawut1990@gmail.com>
2025-03-27 03:13:48 +0700

    เพิ่มโมเดลรถ+ใส่โค้ดขับรถ

--- Assets/Scrip/CarHanler.cs
+++ Assets/Scrip/CarHanler.cs
@@ -1,9 +1,19 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CarHanler : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
+    
+    //Multipliers
+    private float accelerationMultipliers = 3;
+    float breaksMultiplier = 15;
+    float steeringMultipliers = 5;
+    
+    //Input
+    Vector2 input = Vector2.zero;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,12 +28,48 @@ public class CarHanler : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //Apply Acceleration
+        if (input.y > 0)
+            Accelerate();
+        else 
+            rb.drag = 0.2f;
         
+        //Apply Brakes
+        if (input.y < 0)
+            Brake();
+        
+        Steer();
     }
 
     void Accelerate()
     {
-        rb.linearDamping = 0;
+        rb.drag = 0;
+        
+        rb.AddForce(rb.transform.forward * accelerationMultipliers * input.y);
+    }
+
+    void Brake()
+    {
+        //Dont brake unless we are going forward
+        if (rb.velocity.z <= 0)
+            return;
+        
+        rb.AddForce(rb.transform.forward * breaksMultiplier * input.y);
+    }
+
+    void Steer()
+    {
+        if (Mathf.Abs(input.x) > 0)
+        {
+            rb.AddForce(rb.transform.right * steeringMultipliers * input.x);
+        }
+    }
+
+    public void SetInput(Vector2 inputVector)
+    {
+        inputVector.Normalize();
+        
+        input = inputVector;
     }
     
 }
