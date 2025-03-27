19cfbdbcfa09763afee8a6599edcd4498cf2145b
Teddy525252 <ausdawut1990@gmail.com>
2025-03-27 16:22:35 +0700

    แก้ไขโค้ดบังคับรถ+เพิ่มแรงเฉื่อยตอนออกตัว

--- Assets/Script/InputHandler.cs
+++ Assets/Script/InputHandler.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class InputHandler : MonoBehaviour
 {
     [SerializeField]
-    CarHanler carHanler;
+    CarHandler carHanler;
 
     void Update()
     {
