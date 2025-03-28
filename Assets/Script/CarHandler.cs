069fc4ba5710dfeaf72b869193c4b194f71bed35
Hatsapong <hatsapong.api@gmail.com>
2025-03-28 14:56:55 +0700

    เพิ่มรถถอยหลัง

--- Assets/Script/CarHandler.cs
+++ Assets/Script/CarHandler.cs
@@ -11,13 +11,13 @@ public class CarHandler : MonoBehaviour
     Transform gameModel;
 
     //Max values
-    float maxSteerVelocity = 2;
-    float maxForwardVelocity = 30;
+    float maxSteerVelocity = 2f;
+    float maxForwardVelocity = 30f;
 
     //Multipliers
-    float accelerationMultiplier = 3;
-    float breaksMultiplier = 15;
-    float steeringMultiplier = 5;
+    float accelerationMultiplier = 3f;
+    float breaksMultiplier = 15f;
+    float steeringMultiplier = 5f;
 
     //Input
     Vector3 input = Vector3.zero;
@@ -32,16 +32,21 @@ public class CarHandler : MonoBehaviour
     void Update()
     {
         //Rotate car model when "turning"
-        gameModel.transform.rotation = Quaternion.Euler(0, rb.velocity.x * 5, 0);
+        gameModel.transform.rotation = Quaternion.Euler(0, rb.linearVelocity.x * 5, 0);
     }
+
+    // reverseSpeed
+    private float reverseSpeed = 2f;
 
     private void FixedUpdate()
     {
         //Apply Acceleration
         if (input.y > 0)
             Accelerate();
+        else if (input.y < 0)
+            Reverse(); // New function to handle reversing
         else
-            rb.drag = 0.2f;
+            rb.angularDamping = 0.2f;
 
         //Apply Brakes
         if (input.y < 0)
@@ -52,12 +57,17 @@ public class CarHandler : MonoBehaviour
        
     }
 
+    void Reverse()
+    {
+        rb.AddForce(-transform.forward * reverseSpeed, ForceMode.Acceleration);
+    }
+
     void Accelerate()
     {
-        rb.drag = 0;
+        rb.angularDamping = 0;
 
         //Stay within the speed limit
-        if (rb.velocity.z >= maxForwardVelocity)
+        if (rb.linearVelocity.z >= maxForwardVelocity)
             return;
 
         rb.AddForce(rb.transform.forward * accelerationMultiplier * input.y);
@@ -66,38 +76,37 @@ public class CarHandler : MonoBehaviour
     void Brake()
     {
         //Don't brake unless we are going forward
-        if (rb.velocity.z <= 0)
+        if (rb.linearVelocity.z <= 0)
             return;
 
         rb.AddForce(rb.transform.forward * breaksMultiplier * input.y);
     }
 
-    void Steer()
-    {
-        if (Mathf.Abs(input.x) > 0)
+        void Steer()
         {
-            //Move the car sideways
-            float speedBaseSteerLimit = rb.velocity.z / 5.0f;
-            speedBaseSteerLimit = Mathf.Clamp01(speedBaseSteerLimit);
+            if (Mathf.Abs(input.x) > 0)
+            {
+                //Move the car sideways
+                float speedBaseSteerLimit = rb.linearVelocity.z / 5.0f;
+                speedBaseSteerLimit = Mathf.Clamp01(speedBaseSteerLimit);
 
-            rb.AddForce(rb.transform.right * steeringMultiplier * input.x * speedBaseSteerLimit);
+                rb.AddForce(rb.transform.right * steeringMultiplier * input.x * speedBaseSteerLimit);
 
-            //Normalize the X Velocity
-            float normalizedX = rb.velocity.x / maxSteerVelocity;
+                //Normalize the X Velocity
+                float normalizedX = rb.linearVelocity.x / maxSteerVelocity;
 
-            //Ensure that we don't allow it to get bigger than 1 in magnitued. 
-            normalizedX = Mathf.Clamp(normalizedX, -1.0f, 1.0f);
+                //Ensure that we don't allow it to get bigger than 1 in magnitued. 
+                normalizedX = Mathf.Clamp(normalizedX, -1.0f, 1.0f);
 
-            //Make sure we stay within the turn speed limit
-            rb.velocity = new Vector3(normalizedX * maxSteerVelocity, 0, rb.velocity.z);
+                //Make sure we stay within the turn speed limit
+                rb.linearVelocity = new Vector3(normalizedX * maxSteerVelocity, 0, rb.linearVelocity.z);
+            }
+            else
+            {
+                //Auto center car
+                rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, new Vector3(0, 0, rb.linearVelocity.z), Time.fixedDeltaTime * 3);
+            }
         }
-        else 
-        {
-            //Auto center car
-            rb.velocity = Vector3.Lerp(rb.velocity, new Vector3(0, 0, rb.velocity.z), Time.fixedDeltaTime * 3);
-        }
-    }
-
     public void SetInput(Vector3 inputVector)
     {
         inputVector.Normalize();
