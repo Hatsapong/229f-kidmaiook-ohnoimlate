af9f1488fc7b87625f4f1053a53fd8952ed5eb9c
Hatsapong <hatsapong.api@gmail.com>
2025-03-29 13:49:53 +0700

    อุปสรรคพุ่งใส่ทำงาน

--- Assets/Script/Trigger.cs
+++ Assets/Script/Trigger.cs
@@ -5,24 +5,24 @@ public class Trigger : MonoBehaviour
     public GameObject Trap;
     public GameObject Trap2;
     public GameObject Trap3;
-    public Vector3 forceDirection = new Vector3(0, 0, 10); // Adjust force direction and magnitude
-    public float forceMagnitude = 10f; // Adjust as needed
+    public Vector3 forceDirection = new Vector3(0, 0, 10); // Adjust as needed
+    public float forceMagnitude = 15f; // Adjust as needed
 
     private void OnTriggerEnter(Collider other)
     {
-        ApplyConstantForce(Trap);
-        ApplyConstantForce(Trap2);
-        ApplyConstantForce(Trap3);
+        ApplyForce(Trap);
+        ApplyForce(Trap2);
+        ApplyForce(Trap3);
     }
 
-    private void ApplyConstantForce(GameObject trap)
+    private void ApplyForce(GameObject trap)
     {
         if (trap != null)
         {
             Rigidbody rb = trap.GetComponent<Rigidbody>();
             if (rb != null)
             {
-                rb.AddForce(forceDirection.normalized * forceMagnitude, ForceMode.Force);
+                rb.AddForce(forceDirection.normalized * forceMagnitude, ForceMode.Impulse);
             }
             else
             {
